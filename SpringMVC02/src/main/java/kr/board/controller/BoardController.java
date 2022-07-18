d962f2fc4f2bf83d872bdf7104fa52dd18145b98
USER <USER@DESKTOP-HIIP7K6>
2022-07-18 16:54:14 +0900

    SpringMVC02

---
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -0,0 +1,30 @@
+package kr.board.controller;
+
+import java.util.List;
+
+import javax.jws.WebParam.Mode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.board.entity.Board;
+import kr.board.mapper.BoardMapper;
+
+@Controller
+public class BoardController { // new BoardController();
+
+
+	@RequestMapping("/")
+	public String main() {
+		return "main";
+	}
+	
+	
+}
